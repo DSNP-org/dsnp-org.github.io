c44432fb221a85a90d47dce03244ef268aad69ff
V. Claire Olmstead <viol5229@colorado.edu>
2021-07-20 09:24:34 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-670f5fb07f0002035e61.js"
+    "url": "webpack-runtime-a25a41b1cb95a9b8efa4.js"
   },
   {
     "url": "framework-f97fb56dc599ee4ddf0b.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9e506555174bb8818e7bd6ee8286d80e"
+    "revision": "fc6870b52b81d5b6230d273d9b5fef5f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ef318ff420c71f54b8b6.js"
