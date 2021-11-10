e1d54afd96aad962def1803b60eb0e34e61763b5
V. Claire Olmstead <43625033+claireolmstead@users.noreply.github.com>
2021-11-10 10:40:50 -0800

    Feature/dev portal nav/180173171 (#34)
    * dev portal nav bar working for web
    
    * working on mobile
    
    * formatting
    
    Co-authored-by: Claire Olmstead <claireolmstead@Spare-MacBook-Pro.local>
    Co-authored-by: Claire Olmstead <olmsteadclaire@gmail.com>

---
+++ src/components/common/DevPortalNav.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react"
+
+const DevPortalNav = () => {
+    let navItems = [
+        {
+            name: `Introduction`,
+            href: `../dev-portal-introduction/`,
+            class: `DevPortalNav__link`,
+        },
+        {
+            name: `Spec ↗`,
+            href: `https://spec.dsnp.org/`,
+            class: `DevPortalNav__link`,
+            external: true,
+        },
+        {
+            name: `Testnet`,
+            href: `../dev-portal-testnet/`,
+            class: `DevPortalNav__link`,
+        },
+        {
+            name: `SDK`,
+            href: `../dev-portal-sdk/`,
+            class: `DevPortalNav__link`,
+        },
+        {
+            name: `Example Client`,
+            href: `../dev-portal-example-client/`,
+            class: `DevPortalNav__link`,
+        },
+        {
+            name: `Community`,
+            href: `../dev-portal-community/`,
+            class: `DevPortalNav__link`,
+        },
+        {
+            name: `Conduct`,
+            href: `../dev-portal-conduct/`,
+            class: `DevPortalNav__link`,
+        },
+    ]
+
+    const [currentPage, setCurrentPage] = useState()
+
+    useEffect(() => {
+        setCurrentPage(window.location.pathname)
+    }, [window.location.pathname])
+
+    const nav = navItems.map((navItem, index) => {
+        if (navItem.href.includes(currentPage)) {
+            navItems[
+                index
+            ].class = `DevPortalNav__link DevPortalNav__link--active`
+        }
+
+        return (
+            <a
+                className={navItem.class}
+                key={index}
+                href={navItem.href}
+                target={navItem.external && `_blank`}
+            >
+                {navItem.name}
+            </a>
+        )
+    })
+
+    return <nav className="DevPortalNav__block">{nav}</nav>
+}
+
+export default DevPortalNav
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,12 +1,12 @@
-export { default as Layout } from './Layout'
-export { default as PostCard } from './PostCard'
-export { default as FaqPostCard } from './FaqPostCard'
-export { default as IndexPostCard } from './IndexPostCard'
-export { default as Pagination } from './Pagination'
-export { default as Navigation } from './Navigation'
-export { default as Ethos } from './About/EthosPostCard'
-export { default as Governance } from './About/GovernancePostCard'
-export { default as Mission } from './About/MissionPostCard'
-export { default as WhoWeAre } from './About/WhoWeArePostCard'
-export { default as Ecosystem } from './About/EcosystemPostCard'
-
+export { default as Layout } from "./Layout";
+export { default as PostCard } from "./PostCard";
+export { default as FaqPostCard } from "./FaqPostCard";
+export { default as IndexPostCard } from "./IndexPostCard";
+export { default as Pagination } from "./Pagination";
+export { default as Navigation } from "./Navigation";
+export { default as DevPortalNav } from "./DevPortalNav";
+export { default as Ethos } from "./About/EthosPostCard";
+export { default as Governance } from "./About/GovernancePostCard";
+export { default as Mission } from "./About/MissionPostCard";
+export { default as WhoWeAre } from "./About/WhoWeArePostCard";
+export { default as Ecosystem } from "./About/EcosystemPostCard";
