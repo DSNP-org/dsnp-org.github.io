5b4f96d0d1ab587d633ae0accd12b736d9e07872
Claire Olmstead <olmsteadclaire@gmail.com>
2021-11-10 17:17:18 -0800

    styling and content to the about pages. some stuff from old pages deleted

--- src/components/common/About/WhoWeArePostCard.js
+++ src/components/common/About/WhoWeArePostCard.js
@@ -3,19 +3,14 @@ import PropTypes from 'prop-types'
 import WhoWeAreProfiles from "./WhoWeAreProfiles"
 
 const WhoWeArePostCard = ({ whoWeAreCards }) => {
-    const whoWeAreTitle = whoWeAreCards.filter(post => post.node.tags.some(tag => tag.name === `#WhoWeAreTitle`))
     const whoWeAreAdvisoryCouncil = whoWeAreCards.filter(post => post.node.tags.some(tag => tag.name === `#WhoWeAreAdvisoryCouncil`))
     const whoWeAreStaff = whoWeAreCards.filter(post => post.node.tags.some(tag => tag.name === `#WhoWeAreStaff`))
 
-    return <div className="container">
-        <h2 className="TemplatePostCard__title" data-aos="fade-right"
-            data-aos-duration="1400">{whoWeAreTitle[0].node.title}</h2>
-        <div className="WhoWeArePostCard__topBlock">
-            <h3 className="WhoWeArePostCard__blockSubtitle" >Advisors</h3>
-            <WhoWeAreProfiles profiles={whoWeAreAdvisoryCouncil} />
-            <h3 className="WhoWeArePostCard__blockSubtitle" >Team</h3>
-            <WhoWeAreProfiles profiles={whoWeAreStaff} />
-        </div>
+    return <div className="container WhoWeArePostCard__topBlock">
+        <h3 className="WhoWeArePostCard__blockSubtitle" >Advisors</h3>
+        <WhoWeAreProfiles profiles={whoWeAreAdvisoryCouncil} />
+        <h3 className="WhoWeArePostCard__blockSubtitle" >Team</h3>
+        <WhoWeAreProfiles profiles={whoWeAreStaff} />
     </div>
 }
 
